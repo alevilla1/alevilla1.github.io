38885f4cb8478aa1ce3b3865a1542cf95d062905
AV <alejandro.villanueva@razorfish.com>
2023-04-27 12:21:46 -0400

    added second adventure line

--- twine_babylon_v1/main.js
+++ twine_babylon_v1/main.js
@@ -5,12 +5,14 @@ var main = (function(){
     //
     let story = null;
     let display = null;
+    let display2 = null;
     
     pub.start = function(){
         console.log("Initializing");
         //
         story = document.getElementsByClassName("story")[0];
         display = document.getElementsByClassName("display")[0];
+        display2 = document.getElementsByClassName("display2")[0];
         //
         setupListeners();
     }
@@ -22,6 +24,9 @@ var main = (function(){
         display.contentWindow.addEventListener("message", function(event){
             route(event.data)
         }, false);
+        display2.contentWindow.addEventListener("message", function(event){
+            route(event.data)
+        }, false);
     }
 
     route = function(data) {
@@ -29,14 +34,26 @@ var main = (function(){
             case "show_room":
                 tl.to(display, { autoAlpha: 1, duration: 0.5 });
                 break;
+            case "show_spaceship":
+                tl.to(display2, { autoAlpha: 1, duration: 0.5 });
+                break;
             case "start_game1":
                 tl.to(story, { autoAlpha: 0, duration: 0.1 });
                 display.contentWindow.postMessage("display_game1","*")
+                break;
+            case "start_game2":
+                tl.to(story, { autoAlpha: 0, duration: 0.1 });
+                //display2.contentWindow.postMessage("display_game1","*")
                 break;
             case "game1_complete":
                 tl.to(story, { autoAlpha: 1, duration: 0.3 });
                 tl.to(display, { autoAlpha: 0, duration: 0.5 });
                 story.contentWindow.postMessage("goto_end","*");
+                break;
+            case "game2_complete":
+                tl.to(story, { autoAlpha: 1, duration: 0.3 });
+                tl.to(display2, { autoAlpha: 0, duration: 0.5, delay:5 });
+                story.contentWindow.story.show("finishgame")//postMessage("goto_end","*");
                 break;
             case "story_reset":
                 tl.to(display, { autoAlpha: 0, duration: 0.5 });
